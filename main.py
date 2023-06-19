f821c6b0b4cb3c059afd6ff8acbb190242675d46
nbespalovv <99745170+nbespalovv@users.noreply.github.com>
2023-06-19 22:42:34 +0500

    Revert "PM-10 Пример простой задачи для написания документации"

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ db = BotDB('db_file')
 @bot.message_handler(commands=['start'])
 def reply_start_command(message: Message):
     if not db.user_exist(message.chat.id):
-        bot.send_message(message.chat.id, "Приветствие перед регитрацией")
+        bot.send_message(message.chat.id, "Приветсвите перед регитрацией")
         db.user_add(message.chat.id, message.from_user.first_name)
     else:
         bot.send_message(message.chat.id, "Приветстивие зареганого пользователя")
