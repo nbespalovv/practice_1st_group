099dba9e5a771ef0dadd588b198c056df6338575
KarSich <hryhcev@bk.ru>
2023-07-01 09:38:40 +0300

    PM-16 Заменить sqlite на MySQL через SQLAlchemy

--- db.py
+++ db.py
@@ -1,22 +1,51 @@
-import sqlite3
+from sqlalchemy import create_engine, select, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
+    user_first_name = Column(String)
+
 
 class BotDB:
-
-    def __init__(self, db_file):
-        self.connection = sqlite3.connect(db_file, check_same_thread=False)
-        self.cursor = self.connection.cursor()
+    def __init__(self,username,password,db):
+        engine = create_engine(f"mysql+pymysql://{username}:{password}@{db}")
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+        self.users = Users
 
     def user_exist(self, user_id):
-        result = self.cursor.execute("SELECT `user_id` FROM `users` WHERE `user_id`= ?", (user_id,))
-        return bool(len(result.fetchall()))
+        stmt = select(self.users).where(self.users.user_id == user_id)
+        result = self.session.execute(stmt).fetchone()
+        if result is not None:
+            return True
+        else:
+            return False
 
     def user_add(self, user_id, user_first_name):
-        self.cursor.execute("INSERT INTO `users` (`user_id`, `user_first_name`) VALUES (?, ?)", (user_id, user_first_name,))
-        return self.connection.commit()
+        user = self.users(user_id=user_id, user_first_name=user_first_name)
+        self.session.add(user)
+        self.session.commit()
 
     def get_user(self, user_id):
-        result = self.cursor.execute("SELECT `user_id`, `user_first_name` FROM `users` WHERE `user_id`= ?", (user_id,))
-        return result.fetchone()
+        user = self.session.query(self.users).filter_by(user_id=user_id).first()
+        if user:
+            return user.user_first_name
+        else:
+            return None
+
+    def delete_user(self, user_id):
+        user = self.session.query(self.users).filter_by(user_id=user_id).first()
+        if user:
+            self.session.delete(user)
+            self.session.commit()
+            return True
+        else:
+            return False
 
     def close(self):
-        self.connection.close()
+        self.session.close()
