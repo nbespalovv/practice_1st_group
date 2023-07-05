30e96f637b98de2591ff6447e82e189b5b187f29
nbespalovv <Dnevnik1400@mail.ru>
2023-07-05 16:24:13 +0500

    PM-25 Научить парсер запускаться как задача Celery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telebot import TeleBot
 from db import BotDB
 from telebot.types import Message
+from celery_project.parser.tasks import parse_website
 
 token = '6001783957:AAGjtyLX2728zncYkhCDMIq0_MsasMBtOY0'
 bot = TeleBot(token)
@@ -14,7 +15,12 @@ def reply_start_command(message: Message):
         db.user_add(message.chat.id, message.from_user.first_name)
     else:
         bot.send_message(message.chat.id, "Приветстивие зареганого пользователя")
+    parse_website.delay("Том Холланд")
 
 
-bot.infinity_polling()
+
+if __name__ == '__main__':
+    print("Bot started")
+    bot.infinity_polling()
+
 # Вставил комментарий
