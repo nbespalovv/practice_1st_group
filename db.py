a6654957545c731118bc0bef869570e4e4fc0fe9
KarSich <hryhcev@bk.ru>
2023-07-05 13:56:51 +0300

    PM-18 Сделать запись истории сообщений в бд

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine, select, Table, Column, Integer,   String, MetaData, ForeignKey
+from sqlalchemy import create_engine, select, Table, Column, Integer, DateTime, String, MetaData, ForeignKey
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -8,11 +9,22 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True)
-    user_first_name = Column(String(250))
+    user_nickname = Column(String(250))
+
+
+class Logs(Base):
+    __tablename__ = 'logs'
+    log_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_nickname = Column(String(250))
+    user_id = Column(Integer, ForeignKey('users.user_id'))
+    message_text = Column(String(1000))
+    message_time = Column(String(100))
+
 
 class BotDB:
     def __init__(self):
         self.users = Users
+        self.logs = Logs
         f = open('config.txt')
         username = f.readline().replace('\n', '')
         password = f.readline().replace('\n', '')
@@ -23,7 +35,6 @@ class BotDB:
         Session = sessionmaker(bind=engine)
         self.session = Session()
 
-
     def user_exist(self, user_id):
         stmt = select(self.users).where(self.users.user_id == user_id)
         result = self.session.execute(stmt).fetchone()
@@ -32,8 +43,8 @@ class BotDB:
         else:
             return False
 
-    def user_add(self, user_id, user_first_name):
-        user = self.users(user_id=user_id, user_first_name=user_first_name)
+    def user_add(self, user_id, user_nickname):
+        user = self.users(user_id=user_id, user_nickname=user_nickname)
         self.session.add(user)
         self.session.commit()
 
@@ -53,7 +64,11 @@ class BotDB:
         else:
             return False
 
+    def add_log(self, user_nickname, user_id, message_text, message_time):
+        log = self.logs(user_nickname=user_nickname, user_id=user_id, message_text=message_text,
+                        message_time=datetime.fromtimestamp(message_time).strftime('%H:%M - %m.%d.%Y'))
+        self.session.add(log)
+        self.session.commit()
+
     def close(self):
         self.session.close()
-
-
--- main.py
+++ main.py
@@ -9,11 +9,21 @@ db = BotDB()
 
 @bot.message_handler(commands=['start'])
 def reply_start_command(message: Message):
+    updates = bot.get_updates()
     if not db.user_exist(message.chat.id):
-        bot.send_message(message.chat.id, "Приветствие перед регитрацией")
-        db.user_add(message.chat.id, message.from_user.first_name)
+        startstr ="Приветствие перед регитрацией"
+        db.user_add(message.chat.id, message.from_user.username)
     else:
-        bot.send_message(message.chat.id, "Приветстивие зареганого пользователя")
+        startstr = "Приветстивие зареганого пользователя"
+    bot.send_message(message.chat.id, startstr)
+    log_message(updates[-1].message)
+
+
+
+@bot.message_handler(func=lambda message: True)
+def log_message(message: Message):
+    if db.user_exist(message.chat.id):
+        db.add_log(message.from_user.username, message.chat.id, message.text, message.date)
 
 
 bot.infinity_polling()
