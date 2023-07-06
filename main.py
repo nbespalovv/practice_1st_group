5b25020fb29b5bfa13615ddd1e16ee2b8099b6d3
KarSich <hryhcev@bk.ru>
2023-07-06 06:09:54 +0300

    PM-30 Разделение по файлам

--- main.py
+++ main.py
@@ -9,21 +9,18 @@ db = BotDB()
 
 @bot.message_handler(commands=['start'])
 def reply_start_command(message: Message):
-    updates = bot.get_updates()
     if not db.user_exist(message.chat.id):
-        startstr ="Приветствие перед регитрацией"
+        bot.send_message(message.chat.id, "Приветствие перед регитрацией")
         db.user_add(message.chat.id, message.from_user.username)
     else:
-        startstr = "Приветстивие зареганого пользователя"
-    bot.send_message(message.chat.id, startstr)
-    log_message(updates[-1].message)
-
-
+        bot.send_message(message.chat.id, "Приветстивие зареганого пользователя")
+    log_message(message)
 
 @bot.message_handler(func=lambda message: True)
 def log_message(message: Message):
     if db.user_exist(message.chat.id):
         db.add_log(message.from_user.username, message.chat.id, message.text, message.date)
+
 
 
 bot.infinity_polling()
