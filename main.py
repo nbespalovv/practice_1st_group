ec4526498b87a3cdb562efdd3044a8b3b735b6f9
KarSich <hryhcev@bk.ru>
2023-07-10 05:51:09 +0300

    PM-45 менеджер

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from telebot import TeleBot
 from db import BotDB
 from telebot.types import Message, ReplyKeyboardMarkup, KeyboardButton,InlineKeyboardButton, InlineKeyboardMarkup
 from celery_project.parser.tasks import parse_website
+from datetime import datetime
 
 token = '6001783957:AAGjtyLX2728zncYkhCDMIq0_MsasMBtOY0'
 bot = TeleBot(token)
@@ -22,17 +23,21 @@ role_keyboard.add(manager_btn, user_btn)
 user_profile_keyboard = InlineKeyboardMarkup()
 edit_btn = InlineKeyboardButton('Редактировать данные',callback_data="edit_btn")#
 history_btn = InlineKeyboardButton('История',callback_data="history_btn")#
-favorites_btn = InlineKeyboardButton('Избранное',callback_data="favorites_btn")#
+favorites_btn = InlineKeyboardButton('Избранное',callback_data="favourite_btn")#
 balance_btn = InlineKeyboardButton('Баланс',callback_data="balance_btn") #
-buy_tokens_btn = InlineKeyboardButton('Купить токены',callback_data="buy_tokens_btn")
-user_profile_keyboard.add(edit_btn, history_btn, favorites_btn, balance_btn, buy_tokens_btn)
+#buy_tokens_btn = InlineKeyboardButton('Купить токены',callback_data="buy_tokens_btn")
+main_menu_btn = InlineKeyboardButton('В главное меню',callback_data="main_menu_btn")#
+user_profile_keyboard.add(edit_btn, history_btn, favorites_btn, balance_btn)
+user_profile_keyboard.add(main_menu_btn)
 
 manager_profile_keyboard = InlineKeyboardMarkup()
-search_user_btn = InlineKeyboardButton('Найти пользователя',callback_data="search_user_btn")
+search_user_btn = InlineKeyboardButton('Найти пользователя',callback_data="search_user_btn")#
 change_balance_btn = InlineKeyboardButton('Изменить баланс',callback_data="change_balance_btn")
-activity_btn = InlineKeyboardButton('Активность',callback_data="activity_btn")
-new_users_btn = InlineKeyboardButton('Новые пользователи',callback_data="new_users_btn")
+activity_btn = InlineKeyboardButton('Активность',callback_data="activity_btn")#
+new_users_btn = InlineKeyboardButton('Новые пользователи',callback_data="new_users_btn")#
+manager_profile_keyboard.add(edit_btn, history_btn, favorites_btn, balance_btn)
 manager_profile_keyboard.add(search_user_btn, change_balance_btn, activity_btn, new_users_btn)
+manager_profile_keyboard.add(main_menu_btn)
 
 edit_keyboard = InlineKeyboardMarkup()
 username_btn = InlineKeyboardButton('Изменить имя',callback_data="username_btn")#
@@ -55,11 +60,83 @@ def reply_start_command(message: Message):
     #parse_website("Том Холланд")
     #log_message(message)
 
+@bot.callback_query_handler(func=lambda call: call.data == 'activity_btn')
+def activity(call):
+    msg = bot.send_message(call.message.chat.id, 'За сколько дней вы хотите видеть активность?')
+    bot.register_next_step_handler(msg, activity_step, call)
+def activity_step(message, call):
+    time = int(message.text)
+    total_activity = 0
+    users = db.session.query(db.users).all()
+    for user in users:
+        history = user.history
+        if history!=None:
+            for event in history:
+                event_date = datetime(user.event[1].year, user.event[1].month, user.event[1].day)
+                now_date = datetime.now()
+                a =  now_date-event_date
+                if a.days<=time:
+                    total_activity += 1
+    bot.send_message(call.message.chat.id, f'За {time} дней активность составляет {total_activity} запросов',
+                     reply_markup=manager_profile_keyboard)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'change_balance_btn')
+
+def change_balance(call):
+    msg = bot.send_message(call.message.chat.id, 'Введите ID пользователя:')
+    bot.register_next_step_handler(msg, get_user_id_step,call)
+def get_user_id_step(message,call):
+    user_id = message.text
+    msg = bot.send_message(call.message.chat.id, 'Введите новый баланс:')
+    bot.register_next_step_handler(msg, change_balance_step, user_id,call)
+def change_balance_step(message, user_id,call):
+    new_balance = message.text
+    user = db.get_user(user_id)
+    if user:
+        user.points = new_balance
+        db.session.commit()
+        bot.send_message(call.message.chat.id, 'Баланс успешно изменен!',reply_markup=manager_profile_keyboard)
+    else:
+        bot.send_message(call.message.chat.id, 'Пользователь не найден!')
+@bot.callback_query_handler(func=lambda call: call.data == 'new_users_btn')
+def new_users(call):
+    msg = bot.send_message(call.message.chat.id, 'За сколько дней вы хотите видеть новых пользователей?')
+    bot.register_next_step_handler(msg, new_users_step, call)
+def new_users_step(message, call):
+    time = int(message.text)
+    new_users = 0
+    users = db.session.query(db.users).all()
+    for user in users:
+        reg_date = datetime(user.reg_data.year, user.reg_data.month, user.reg_data.day)
+        now_date = datetime.now()
+        a = now_date - reg_date
+        if a.days <= time:
+            new_users += 1
+    bot.send_message(call.message.chat.id, f'За {time} дней появилось {new_users} новых пользователей')
+    profile(call)
+
+
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'search_user_btn')
+def search_user(call):
+    msg = bot.send_message(call.message.chat.id, 'Введите id пользователя')
+    bot.register_next_step_handler(msg, search_step, call)
+
+def search_user_step(message, call):
+    id = message.text
+    user = db.get_user(id)
+    if db.user_exist(id):
+        bot.send_message(call.message.chat.id,
+                     f'Информация о пользователе  {id}:\nИмя - {user.username}\nРоль - {user.role}\nБаланс - {user.points}\nВозраст - {user.age}\nПочта - {user.email}\nТелефон - {user.phone}\nДата регистрации - {datetime(user.reg_data.year, user.reg_data.month, user.reg_data.day)}')
+    else: bot.send_message(call.message.chat.id,"Пользователь не найден!")
+    profile(call)
+
+
 @bot.callback_query_handler(func=lambda call: call.data == 'search_btn')
 def search(call):
     msg = bot.send_message(call.message.chat.id, 'Введите имя актера')
     bot.register_next_step_handler(msg, search_step, call)
-
 
 def search_step(message, call):
     actor = message.text
@@ -67,6 +144,9 @@ def search_step(message, call):
     bot.send_message(message.chat.id, 'Поиск начат!!')
     profile(call)
 
+@bot.callback_query_handler(func=lambda call: call.data == 'main_menu_btn')
+def show_main_menu(call):
+    bot.send_message(call.message.chat.id, 'Главное меню', reply_markup=main_menu)
 @bot.callback_query_handler(func=lambda call: call.data == 'manager_btn')
 def manager(call):
     db.user_add(call.message.chat.id,call.message.chat.username,'Manager')
@@ -89,6 +169,7 @@ def profile(call):
 def balance(call):
     user =db.get_user(call.message.chat.id)
     bot.send_message(call.message.chat.id, f'Ваш баланс - {user.points} токенов.')
+    profile(call)
 @bot.callback_query_handler(func=lambda call: call.data == 'edit_btn')
 def edit(call):
     bot.send_message(call.message.chat.id, 'Режим редактирования',reply_markup=edit_keyboard)
@@ -143,24 +224,25 @@ def show_history(call):
     history = user.history
 
     text = 'Ваша история:\n'
+    if history!=None:
+        for event in history:
+            text += f'{event[0]} - {event[1]}\n'
+            bot.send_message(call.message.chat.id, text, parse_mode='HTML')
+    else:
+        bot.send_message(call.message.chat.id, "История пуста!")
 
-    for search, date in history.items():
-        text += f'{search} - {date["date"]}\n'
-
-    bot.send_message(call.message.chat.id, text, parse_mode='HTML')
 
 @bot.callback_query_handler(func=lambda call: call.data == 'favourite_btn')
 def show_favourite(call):
     user = db.get_user(call.message.chat.id)
-    history = user.history
-    text = 'Ваша история:\n'
-
-
-    for search, date in history.items():
-        text += f'{search} - {date["date"]}\n'
-
-    bot.send_message(call.message.chat.id, text, parse_mode='HTML')
-
+    favourite = user.favourite
+    text = 'Ваше избранное:\n'
+    if favourite != None:
+        for event in favourite:
+            text += f'{event[0]} - {event[1]}\n'
+            bot.send_message(call.message.chat.id, text, parse_mode='HTML')
+    else:
+        bot.send_message(call.message.chat.id, "Избранные пусты!")
 @bot.message_handler(func=lambda message: True)
 def log_message(message: Message):
     if db.user_exist(message.chat.id):
